c120f761dba82db33ffbeb507813ab73a8fc49e1
Phuoc Do <dnprock@gmail.com>
2015-09-04 00:18:10 -0700

    Fix bugs and add options

--- vida/__init__.py
+++ vida/__init__.py
@@ -1,3 +1,3 @@
 from vida import *
 
-VERSION = '0.2'
+VERSION = '0.3'
--- vida/vida.py
+++ vida/vida.py
@@ -1,9 +1,11 @@
 from IPython.core.display import HTML
 import json
 
-def show(id, data):
-    HTML('<script type="text/javascript" src="https://rawgit.com/vidalab/isender/master/isender.js"></script>' +
-    '<iframe src="https://vida.io/embed/' + id + '?ext_data=true" width="800" height="500" seamless frameBorder="0" scrolling="no" id="vida-embed"></iframe>' +
+def show(id, data, options={}):
+    opts = {'width': 800, 'height': 500, 'scrolling': 'no'}.copy()
+    opts.update(options)
+    return HTML('<script type="text/javascript" src="https://rawgit.com/vidalab/isender/master/isender.js"></script>' +
+    '<iframe src="https://vida.io/embed/' + id + '?ext_data=true" width="' + str(opts['width']) + '" height="' + str(opts['height']) + '" seamless frameBorder="0" scrolling="' + opts['scrolling'] + '" id="vida-embed"></iframe>' +
     '<script type="text/javascript">' +
     'document.getElementById("vida-embed").onload = function() {' +
     'var dataObj = ' +
