e4ccadb73c3d0b1388a321fb4e6802631c680649
Phuoc Do <dnprock@gmail.com>
2015-09-03 23:42:21 -0700

    Update lib

--- vida/__init__.py
+++ vida/__init__.py
@@ -1,3 +1,3 @@
-VERSION = '0.1'
+from vida import *
 
-import vida
+VERSION = '0.2'
--- vida/vida.py
+++ vida/vida.py
@@ -1,18 +1,14 @@
 from IPython.core.display import HTML
 import json
 
-class vida:
-    def __init__(self):
-        self.name = 'vida'
-
-    def show(id, data):
-        HTML('<script type="text/javascript" src="https://rawgit.com/vidalab/isender/master/isender.js"></script>' +
-        '<iframe src="https://vida.io/embed/' + id + '?ext_data=true" width="800" height="500" seamless frameBorder="0" scrolling="no" id="vida-embed"></iframe>' +
-        '<script type="text/javascript">' +
-        'document.getElementById("vida-embed").onload = function() {' +
-        'var dataObj = ' +
-        json.dumps(data) + ';' +
-        'var sender = new Vida.ISender("vida-embed");' +
-        'sender.postRender(dataObj);' +
-        '}' +
-        '</script>')
+def show(id, data):
+    HTML('<script type="text/javascript" src="https://rawgit.com/vidalab/isender/master/isender.js"></script>' +
+    '<iframe src="https://vida.io/embed/' + id + '?ext_data=true" width="800" height="500" seamless frameBorder="0" scrolling="no" id="vida-embed"></iframe>' +
+    '<script type="text/javascript">' +
+    'document.getElementById("vida-embed").onload = function() {' +
+    'var dataObj = ' +
+    json.dumps(data) + ';' +
+    'var sender = new Vida.ISender("vida-embed");' +
+    'sender.postRender(dataObj);' +
+    '}' +
+    '</script>')
